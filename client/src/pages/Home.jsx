80d480ea21b726e7a5bd5fed418551a0eec8cb55
Zartaroth <jsalazarbenavente2014@gmail.com>
2024-06-05 11:41:35 -0500

    Implemntacion Template Pagina Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,40 +1,64 @@
-import { useAuth } from "../auth/AuthProvider";
-import "../styles/Home.css";
-// import Main from "../components/Common/Main";
+import * as React from "react";
 
-function Home() {
-  const auth = useAuth();
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import getDashboardTheme from "../components/getDashboardTheme";
+import ToggleCustomTheme from "../components/internals/components/ToggleCustomTheme";
+import Copyright from "../components/internals/components/Copyright";
+import Navbar from "../components/components/Navbar";
+import Header from "../components/components/Header";
+import MainGrid from "../components/components/MainGrid";
+
+export default function Home() {
+  const [mode, setMode] = React.useState("light");
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const dashboardTheme = createTheme(getDashboardTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
+  };
 
   return (
-    <>
-      <header>
-        {/* <!-- Navbar --> */}
-        {/* <NavBar /> */}
-
-        {/* <!-- Banner --> */}
-        <div className="banner">
-          <div className="overlay">
-            <div className="TextContainer">
-              <h1 className="title">
-                Bienvenido {auth.getUser()?.firstName || ""}
-              </h1>
-              <p className="subtitle">
-                Lorem Ipsumes simplemente el texto de relleno de las imprentas y
-                archivos de texto. Lorem Ipsum ha sido el texto de relleno
-                estándar de las industrias desde el año 1500, cuando un impresor
-                (N. de Tu vieja) desconocido usó una galería de textos y los
-                mezcló de tal manera que logró hacer un libro de textos
-                especimen.
-              </p>
-            </div>
-          </div>
-        </div>
-      </header>
-
-      {/* <!-- Contenido principal --> */}
-      {/* <Main /> */}
-    </>
+    <ThemeProvider theme={showCustomTheme ? dashboardTheme : defaultTheme}>
+      <CssBaseline />
+      <Box sx={{ display: "flex" }}>
+        <Navbar mode={mode} toggleColorMode={toggleColorMode} />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: "background.default",
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          {/* Main content */}
+          <Container
+            maxWidth="xl"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              py: { xs: 14, sm: 16 },
+              gap: 2,
+            }}
+          >
+            <Header />
+            <MainGrid />
+            <Copyright sx={{ my: 4 }} />
+          </Container>
+        </Box>
+        <ToggleCustomTheme
+          showCustomTheme={showCustomTheme}
+          toggleCustomTheme={toggleCustomTheme}
+        />
+      </Box>
+    </ThemeProvider>
   );
 }
-
-export default Home;
